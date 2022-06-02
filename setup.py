9d1374db19ea97f53079b438aaec8f7f13184a97
Pranav Raikote <pranavraikote@gmail.com>
2022-06-02 17:02:51 +0530

    Updated dependency and version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="plexflo",
-    version="1.0.0",
+    version="1.0.1",
     author="Plexflo",
     author_email="hello@plexflo.com",
     description="Open-source Python wrapper around Plexflo's apps, APIs, and algorithms that help researchers, engineers and energy companies build intelligence, apps, and dashboards to accelerate clean-tech adoption across all communities.",
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/plexflo/plexflo",
     packages=find_packages(),
     include_package_data=True,
-    install_requires = ['numpy', 'pandas', 'sklearn', 'pathlib', 'tensorflow', 'matplotlib'],
+    install_requires = ['numpy', 'pandas', 'sklearn', 'openpyxl', 'pathlib', 'tensorflow', 'matplotlib'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
