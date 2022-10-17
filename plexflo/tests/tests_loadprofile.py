0afa36bcd260504b805b038f500b4f4ce7aafafb
Pranav Raikote <pranavraikote@gmail.com>
2022-10-17 17:53:22 +0530

    Added a basic test under loadprofiles

--- plexflo/tests/tests_loadprofile.py
+++ plexflo/tests/tests_loadprofile.py
@@ -1,3 +1,9 @@
 import unittest
 
-from plexflo.loadprofiles import profiles
+from plexflo.loadprofiles.profiles import show, use, impact_of_policy_every_year_on_peak_kW
+
+def test_load():
+
+    df = use()
+    if 'kW_multiplier' in df.columns and df.shape[0] == 24:
+        return True
